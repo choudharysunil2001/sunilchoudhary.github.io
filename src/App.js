c3d9561e029c683a3c99aa074f4ef604f53ec4d8
choudharysunil2001 <sunilchoudhary83850046@gmail.com>
2023-05-13 17:39:04 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Home } from './Home';
+import TopNav from './TopNav';
+
+
+function App() {
+  return (
+    <div className="App">
+  <TopNav></TopNav>
+    <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TopNav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './TopNav.css'
+
+const TopNav = () => {
+  return (
+    <>
+      <div className='navbar'>
+
+
+        <h1 id='logo'>Sunil Choudhary</h1>
+
+      </div>
+      <div className='navlink'>
+        <a href=''>Home</a>
+        <a href=''>About Us</a>
+        <a href=''>Contect</a>
+        <a href=''>Exeprience</a>
+        <a href=''>Project</a>
+        <a href=''>Gallery</a>
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default TopNav
